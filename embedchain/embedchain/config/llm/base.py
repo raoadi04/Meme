9b765c97d38e474f448fb16eb6a601e31b3b6608
PranavPuranik <pranavpuranik10@gmail.com>
2024-07-05 23:11:50 -0500

    removing setup db

--- embedchain/embedchain/config/llm/base.py
+++ embedchain/embedchain/config/llm/base.py
@@ -1,5 +1,5 @@
+import json
 import logging
-import os
 import re
 from string import Template
 from typing import Any, Mapping, Optional, Dict, Union
@@ -7,7 +7,6 @@ from typing import Any, Mapping, Optional, Dict, Union
 import httpx
 
 from embedchain.config.base_config import BaseConfig
-from embedchain.core.db.database import init_db, setup_engine
 from embedchain.helpers.json_serializable import register_deserializable
 
 
@@ -210,11 +209,6 @@ class BaseLlmConfig(BaseConfig):
 
         if prompt is None:
             prompt = DEFAULT_PROMPT_TEMPLATE
-
-        # Initialize the metadata db for the app here since llmfactory needs it for initialization of
-        # the llm memory
-        setup_engine(database_uri=os.environ.get("EMBEDCHAIN_DB_URI"))
-        init_db()
 
         self.number_documents = number_documents
         self.temperature = temperature
